21fcf42506462bb413bc8b03906bb1b0d0821bb4
DS-Garnett-Dan-Nu <garnett.dn@gmail.com>
2023-03-04 23:10:15 +0630

    Basic Walking Animation, Gun Controls, A tat'bit of code refining

--- test/test_gdscripts/FCC.gd
+++ test/test_gdscripts/FCC.gd
@@ -1,18 +1,21 @@
 extends Spatial
 
 
-# Declare member variables here. Examples:
-# var a = 2
-onready var gun_control = $"fcc/Upper Base/Duel EFC 100g and others/Null2/Gun"
+#Node Variables
+onready var gun_control = $"fcc/Upper Base/Gun" #Gun's Base Elevation Control
+onready var animations = $Anis #Animation Controls
+
+#These nodes are for Fire Control, i.e. both feet are need to be on the floor to Fire.
+onready var left_feet = $"fcc/Left Feet +50cm Y is Floor" 
+onready var right_feet = $"fcc/Right Feet +50cm Y is Floor"
 
 
-# Called when the node enters the scene tree for the first time.
+#No use for now, butt too lazy to write again, so imma keep this :3
 func _ready():
 	pass # Replace with function body.
 
 
-
-
+#Physics Processes eh?
 func _physics_process(delta):
 	
 	#Gun Elevations
@@ -24,6 +27,34 @@ func _physics_process(delta):
 	#Gun Elevations Control cuz godot's physics_process always overshot the degress and gets stuck :3
 	if Input.is_action_pressed("GE_Up") and gun_control.rotation_degrees.z > 208:
 		gun_control.rotation_degrees.z = 209
-		
 	if Input.is_action_pressed("GE_Down") and gun_control.rotation_degrees.z < -28:
 		gun_control.rotation_degrees.z = -29
+
+	#Gun Fire Control, i.e. both feet are need to be on the floor to Fire.
+	if Input.is_action_just_pressed("Fire"):
+		if left_feet.translation.y < - 1.0 and right_feet.translation.y < - 1.0:
+			print("Fire!")
+		else:
+			print("WARNING! Both Feet are needed to be on the ground to Fire.")
+
+
+
+
+
+
+
+	#For Movement Animations
+	#If both buttons are pressed together, the animation will stop.
+	if  Input.is_action_pressed("Go_Left") and Input.is_action_pressed("Go_Right"):
+		animations.stop(false)
+	else:
+		#Normal Movement Animations
+		if Input.is_action_pressed("Go_Right"):
+			animations.play("walking")
+		if Input.is_action_just_released("Go_Right"):
+			animations.stop(false)
+			
+		if Input.is_action_pressed("Go_Left"):
+			animations.play_backwards("walking")
+		if Input.is_action_just_released("Go_Left"):
+			animations.stop(false)
--- test/test_gdscripts/test_player.gd
+++ test/test_gdscripts/test_player.gd
@@ -2,10 +2,12 @@ extends KinematicBody
 
 
 # Declare member variables here. Examples:
-export var speed := 15.0
+export var ori_speed = 5
+
 export var jump_strength := 50.0
 export var gravity := 75.0
 
+var speed = ori_speed
 var _velocity := Vector3.ZERO
 var _snap_vector := Vector3.DOWN
 
@@ -23,7 +25,7 @@ func _physics_process(delta: float) -> void:
 	
 	#Movements
 	var move_direction := Vector3.ZERO
-	move_direction.x = Input.get_action_strength("Go_Left") - Input.get_action_strength("Go_Right")
+	move_direction.x = Input.get_action_strength("Go_Right") - Input.get_action_strength("Go_Left")
 	#move_direction.z = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") #this is for z movement, if needed
 	
 	_velocity.x = move_direction.x * speed
@@ -33,9 +35,11 @@ func _physics_process(delta: float) -> void:
 	var is_jumping := is_on_floor() and Input.is_action_just_pressed(("jump"))
 	
 	if is_jumping:
+		speed = ori_speed*3
 		_velocity.y = jump_strength
 		_snap_vector = Vector3.ZERO
 	elif just_landed:
+		speed = ori_speed
 		_snap_vector = Vector3.DOWN
 		
 	_velocity = move_and_slide_with_snap(_velocity, _snap_vector, Vector3.UP, true)
@@ -46,14 +50,17 @@ func _physics_process(delta: float) -> void:
 	#Camera... Rotating eh?
 	if  Input.is_action_pressed("Go_Left") and Input.is_action_pressed("Go_Right"):
 		pass
-	elif  Input.is_action_pressed("Go_Left") and _spring_arm.rotation_degrees.y > -5:
+	elif  Input.is_action_pressed("Go_Right") and _spring_arm.rotation_degrees.y > -5:
 		_spring_arm.rotation_degrees.y -= 1.5
+		_spring_arm.translation.x += 1
 
-	elif  Input.is_action_pressed("Go_Right") and _spring_arm.rotation_degrees.y < 5:
+	elif  Input.is_action_pressed("Go_Left") and _spring_arm.rotation_degrees.y < 5:
 		_spring_arm.rotation_degrees.y += 1.5
+		_spring_arm.translation.x -= 1
 
-
-		
+	if  Input.is_action_pressed("reset_camera"):
+		_spring_arm.rotation_degrees.y = 0
+		_spring_arm.translation.x = 0
 	
 	
 #func _process(delta):
